5d2b199588407ff596f4ce0f3c21f70299aa258f
roeuihyun <roeuihyun@gmail.com>
2024-03-29 22:32:33 +0900

    Design patterns using functional programming 3 patterns

---
+++ src/com/functional/java/DesignPatternsUsingFunctionalProgramming1.java
@@ -0,0 +1,9 @@
+package com.functional.java;
+
+public class DesignPatternsUsingFunctionalProgramming1 {
+	
+	// 생성 패턴 (Creational Patterns) - 오브젝트의 생성에 관련된 패턴
+	// 구조 패턴 (Strutural Patterns) - 상속을 이용해 클래스/오브젝트를 조합하여 더 발전된 구조로 만드는 패턴
+	// 행동 패턴 (Behavioral Patterns) - 필요한 작업을 여러 객체를 분해하여 객체간 결합도를 줄이게 해주는 패턴
+
+}
